480c158901fc3f8fba0e5bd218e36b2f5b9bf34f
Rhytham Negi <rhythamnegi@Rhythams-MacBook-Air.local>
2026-05-03 12:41:51 +0530

    class 02 : useState, useEffect

--- react-refresher/src/App.jsx
+++ react-refresher/src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App(){
     // Only need to use TOP
 
+    const [darkMode, setDarkMode] = useState(false)
+
+    function onToggleTheme(){
+        setDarkMode(!darkMode)
+        console.log(darkMode)
+    }
+
     const [count, setCount] = useState(0)
 
+    // without dep Array
+    // useEffect(()=>{
+    //     console.log("I am Under useEffect")
+    // })
+
+    // with empty dep array
+    // useEffect(()=>{
+    //     console.log("I am Under useEffect with empty dep array")
+    // },
+    // [])
+
+    // with dep array
+    useEffect(
+        ()=> {
+            console.log("I am Under useEffect with dep array")
+        },[count]
+    )
+
     return (
-        <div>
-            <h1>{count}</h1>
-            <button onClick={()=>setCount(count+1)}>➕</button>
-            <button onClick={()=>setCount(count-1)}>➖</button>
+        <div 
+        style = {{
+            height : "100vh",
+            backgroundColor : darkMode ? "#1212" : "#ffff",
+        }}>
+            <button onClick={onToggleTheme}>
+                Toggle Theme
+            </button>
+            <button onClick={()=>{setCount(count +1)}}>
+                Increment
+            </button>
         </div>
     )
 }
